5473b29e112ff4c0224f517fe390922917ff1b0d
ekzGuille <guillermosesesantos@hotmail.com>
2019-07-25 18:55:06 +0200

    Añadido cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-
+const cors = require('cors');
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
 const data = require('./routes/data');
 
 const app = express();
-
+app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
 
