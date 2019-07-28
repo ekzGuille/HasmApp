c01e8b69af06bf5d464c7e121b249113d2f24340
ekzGuille <guillermosesesantos@hotmail.com>
2019-07-29 01:03:24 +0200

    Añadido CORS + control de errores ⚠

--- client/index.js
+++ client/index.js
@@ -1,5 +1,11 @@
-// const API_URL = `https://apihasmapp.now.sh/api`;
-const API_URL = `http://localhost:5000/api`;
+const API_URL = `https://hasmappapi.ekzguille.now.sh`;
+// const API_URL = `http://localhost:5000;
+const cors_headers = {
+  mode: 'cors',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
+};
 const input = document.getElementById('texto');
 const cargando = document.getElementById('cargandoGif');
 const noEncontrado = document.getElementById('noEncontrado');
@@ -11,23 +17,26 @@ async function buscar(e) {
   }
   cargando.style.display = 'block';
   noEncontrado.style.display = 'none';
-  // const jsonData = await fetch(`${API_URL}/ver_imagenes/${texto}`);
-  const jsonData = await fetch(`${API_URL}/ver_imagenes/${texto}`);
-  const { docs } = await jsonData.json();
-  
-  const div = document.getElementById('resultados');
-  div.innerHTML = '';
-  
-  cargando.style.display = 'none';
-  if(docs.length === 0) {
+  const jsonData = await fetch(`${API_URL}/api/ver_imagenes/${texto}`, cors_headers);
+  try {
+    const { docs } = await jsonData.json();
+    const div = document.getElementById('resultados');
+    div.innerHTML = '';
+    cargando.style.display = 'none';
+    if (docs.length === 0) {
+      noEncontrado.style.display = 'block';
+      return;
+    }
+    docs.map(({ fileName, image }) => {
+      const img = document.createElement('img');
+      img.src = image;
+      img.alt = fileName;
+      img.className = 'imagen'
+      div.appendChild(img);
+    });
+  } catch (e) {
+    console.error(e);
+    cargando.style.display = 'none';
     noEncontrado.style.display = 'block';
-    return;
   }
-  docs.map(({ fileName, image }) => {
-    const img = document.createElement('img');
-    img.src = image;
-    img.alt = fileName;
-    img.className = 'imagen'
-    div.appendChild(img);
-  });
 }
